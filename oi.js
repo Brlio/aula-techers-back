1b68fc36c2cfbabe630b2772b2582d7d33d1304b
Bruno <brunohenriquerleao331@gmail.com>
2026-04-10 18:00:08 -0300

    ds

---
+++ oi.js
@@ -0,0 +1,14 @@
+import http from 'node:http'
+import fs from 'node:fs/promises'
+
+const server = http.createServer(async (request, response)=>{
+    const {url, method} = request
+    if(url == '/' && method == 'GET'){
+        const template = await fs.readFile('./oi.html', 'utf-8')
+        response.setHeader('Content-Type', 'text/html; charset=utf-8;')
+        response.writeHead(200)
+        response.end(template)
+    }
+
+})
+server.listen(3030)
