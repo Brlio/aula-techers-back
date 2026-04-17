864f32ed56d5914a5ccf57c9de641ce211bae12a
Bruno <brunohenriquerleao331@gmail.com>
2026-04-17 17:22:21 -0300

    oi

--- oi.js
+++ oi.js
@@ -5,9 +5,14 @@ const server = http.createServer(async (request, response)=>{
     const {url, method} = request
     if(url == '/' && method == 'GET'){
         const template = await fs.readFile('./oi.html', 'utf-8')
+        const dadosBanco = await fs.readFile('./inventario.json', 'utf-8')
+        const inventario = JSON.parse(dadosBanco)
+        console.log("Dados carregados:", inventario)
+        const linhaTeste = "<tr> <td> 1 </td>  <td>Espelho magico </td> <td> item magico </td> <td> 1 </td> </tr>"
+        const paginaFinal = template.replace("{{TABELA}}", linhaTeste)
         response.setHeader('Content-Type', 'text/html; charset=utf-8;')
         response.writeHead(200)
-        response.end(template)
+        response.end(paginaFinal)
     }
 
 })
